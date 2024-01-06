8c9ccbb629ab56e50d99dccb2cb50be7f0f98479
CeciliaChen <cecilia@pdotf.com>
2024-01-06 17:29:44 -0500

    Implement login user API that returns PASETO or JWT access token in Go

--- api/main_test.go
+++ api/main_test.go
@@ -1,10 +1,26 @@
 package api
 
 import (
-	"github.com/gin-gonic/gin"
 	"os"
 	"testing"
+	"time"
+
+	db "github.com/CeciliaChen/simplebank/db/sqlc"
+	"github.com/CeciliaChen/simplebank/util"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+
+	return server
+}
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
